d6f071719b495316bb7a191ce3faf1ccc4c99c2c
vmanh131 <99304738+vmanh131@users.noreply.github.com>
2022-07-16 18:17:16 +0700

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE `todo`;
+  
+USE `todo`;
+  
+CREATE TABLE `todo`.`todo_list` ( `id` INT NOT NULL AUTO_INCREMENT , 
+`name` VARCHAR(100) NOT NULL , 
+`date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+PRIMARY KEY (`id`)) ENGINE = InnoDB;
